5c8259893d68f151b2a90a32ab7d5a3573fd5fa0
MartiPJ <mugiwara1209@gmail.com>
2024-03-05 11:19:25 -0600

    configuracion del app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().default(5432),
+        DB_PORT: Joi.string().required(), 
+        DB_NAME: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+        MESSAGE: Joi.string().required(),
+
+      })
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'libro',
+      password: '1234',
+      database: 'libros',
+      autoLoadEntities: true,
+      synchronize: false,      
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
