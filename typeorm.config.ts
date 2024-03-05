d577192c6744d0f617702c8732e8705bfb5a0a77
MartiPJ <mugiwara1209@gmail.com>
2024-03-05 11:06:48 -0600

    configuracion del typeorm

---
+++ typeorm.config.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'libro',
+  password: '1234',
+  database: 'libros',
+  synchronize: false,
+  //entities: ['src/**/*.entity.ts'],
+  //migrations: ['./src/migrations/*.ts'],
+});
