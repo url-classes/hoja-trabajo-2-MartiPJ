883a6c0ed85cb2273e7acda8d43a32ad748ff9c1
MartiPJ <mugiwara1209@gmail.com>
2024-03-05 11:30:58 -0600

    se configuro el app service y el app.controller.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor( private configService: ConfigService){}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getMessage(): string{
+    return this.configService.get('MESSAGE');
   }
 }
