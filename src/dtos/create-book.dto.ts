f978c65877c0e26fa6f4b60d005e2b936bdb4f0a
MartiPJ <mugiwara1209@gmail.com>
2024-03-05 19:52:14 -0600

    configuracion de create y uptdate, class Validator, mapped-types

--- src/dtos/create-book.dto.ts
+++ src/dtos/create-book.dto.ts
@@ -1,6 +1,22 @@
+import{
+    IsString,
+    IsOptional,
+    MaxLength,
+    MinLength,
+    
+} from 'class-validator';
+
 export default class CreateBookDto{
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
     title: string;
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(255)
     gender: string;
+    
     yearpublication: Date;
 
 }
